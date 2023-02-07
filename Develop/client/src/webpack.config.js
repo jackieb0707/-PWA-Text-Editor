45fcf3f20d8e4fbd6219b79e1c82124295f9a9c2
jackieb0707 <jackiebell446@yahoo.com>
2023-02-07 10:01:49 -0600

    Modify code

--- Develop/client/src/webpack.config.js
+++ Develop/client/src/webpack.config.js
@@ -11,19 +11,67 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/install.js',
+      editor: './src/js/install.js',
+      header: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Webpack plugin that generates our html files and injects our bundles
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+      // Injects our custom service worker
+      new InjectManifest ({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      // create manifest.json file
+      new WebpackPwaManifest ({
+        fingerprints: false,
+        inject: true,
+        name: " text editor",
+        short_name: 'JATE',
+        description: "just another text editor",
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src:path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
+       //css loaders
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader','css-loader']
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // use babel-loader in order to use ES6
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presents: ['@babel/preset-env'],
+              plugings: ['@babel/plugin-proposal-object-rest-spread','@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
